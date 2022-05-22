9661242843bd72e9429e9a2c4605c26ab7c585ae
Kevin.Q <k.queyroi@gmail.com>
2022-05-22 18:38:36 +0200

    date correcter

--- resources/views/front/quizResult.blade.php
+++ resources/views/front/quizResult.blade.php
@@ -7,7 +7,7 @@ BigScreen - Questionnnaire
 @section('content')
 <div class="card-header mb-2 py-3">
     <img class ="mx-auto mb-4 mt-3 fade-in-fade-out" src="{{asset('img/logo_bigscreen.png')}}" alt="logo bigscreen">
-    <p class ="date text-blue text-center" >Vous trouverez ci-dessous les réponses que vous avez apportées à notre sondage le "{{$answers[0]->created_at->isoFormat('DD.mm.YYYY à HH.mm')}}"</p>
+    <p class ="date text-blue text-center" >Vous trouverez ci-dessous les réponses que vous avez apportées à notre sondage le "{{$answers[0]->created_at}}"</p>
 </div>        
 <div class="d-flex align-items-center justify-content-center vh-50">
     <div class="card-body text-white bg-dark rounded my-3 mb-3 mx-auto " style="max-width: 60rem;">
@@ -21,7 +21,7 @@ BigScreen - Questionnnaire
             @foreach ($answers as $answer)
             <tbody>
                 <tr>
-                    <td>{{ $questions[$answer->question_id-1]->title }}</td>
+                    <td class ="text-info">{{ $questions[$answer->question_id-1]->title }}</td>
                     <td class="label">{{ $questions[$answer->question_id-1]->description }}</td>
                     <td>{{$answer->answer}}</td>
                 </tr>
